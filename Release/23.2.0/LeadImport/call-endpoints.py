66a62f0e3dc42be117b8a5f993e1a037a3037ade
Abhijeet Bodhankar <48222006+abbodh@users.noreply.github.com>
2023-07-13 16:58:37 +0530

    Update call-endpoints.py

--- Release/23.2.0/LeadImport/call-endpoints.py
+++ Release/23.2.0/LeadImport/call-endpoints.py
@@ -1,10 +1,10 @@
 
 #script will determine the url to call based on the input ENVIRONMENT
 import requests
-api_url = "https://leads.api.io.dev1.velocify.net/status"
+api_url = "http://api.open-notify.org/astros.json"
 response = requests.get(api_url)
 responseStatusCode = str(response.status_code)
 jsonResponse = response.json()
 print("url: " + api_url)
 print("status code: " + responseStatusCode)
-print("status: " + jsonResponse["status"])
+print("status: " + jsonResponse)
