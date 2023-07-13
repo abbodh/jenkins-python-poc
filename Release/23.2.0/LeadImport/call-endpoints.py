4afd5c2172f4b032d8c9d5b4ccd65a2b7a420fa7
Abhijeet Bodhankar <48222006+abbodh@users.noreply.github.com>
2023-07-13 15:25:55 +0530

    Update call-endpoints.py

--- Release/23.2.0/LeadImport/call-endpoints.py
+++ Release/23.2.0/LeadImport/call-endpoints.py
@@ -1,6 +1,10 @@
 
 #script will determine the url to call based on the input ENVIRONMENT
-
-# This program prints Hello, world!
-
-print('Hello, world!')
+import requests
+api_url = "https://leads.api.io.dev1.velocify.net/status"
+response = requests.get(api_url)
+responseStatusCode = str(response.status_code)
+jsonResponse = response.json()
+print("url: " + api_url)
+print("status code: " + responseStatusCode)
+print("status: " + jsonResponse["status"])
