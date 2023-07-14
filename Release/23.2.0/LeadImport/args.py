949778792c2efb147ee9d8c37115d94a13ff809b
Abhijeet Bodhankar <48222006+abbodh@users.noreply.github.com>
2023-07-14 13:45:21 +0530

    Update args.py

--- Release/23.2.0/LeadImport/args.py
+++ Release/23.2.0/LeadImport/args.py
@@ -4,4 +4,4 @@ def argstoparse():
     parser = argparse.ArgumentParser(allow_abbrev=False)
     parser.add_argument('--environment', action='store', type=str, required=True)
     args = parser.parse_args()
-return args
+    return args
