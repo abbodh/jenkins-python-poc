de2d17cd6b6ea1ec616127f19c8041e85cd693b5
Abhijeet Bodhankar <48222006+abbodh@users.noreply.github.com>
2023-07-14 13:23:25 +0530

    Update call-endpoints.py with env arg

--- Release/23.2.0/LeadImport/call-endpoints.py
+++ Release/23.2.0/LeadImport/call-endpoints.py
@@ -1,11 +1,17 @@
 # script will determine the url to call based on the input ENVIRONMENT which is read by sys.args
-import sys
+import sys, argparse
 import requests
+
+parser = argparse.ArgumentParser()
+args=parser.parse_args()
+
+print("running for environment: " + args.env)
+
+print("hit url: " + api_url)
 api_url = "http://api.open-notify.org/astros.json"
 response = requests.get(api_url)
 responseStatusCode = str(response.status_code)
 jsonResponse = response.json()
-print("Environment:", sys.argv[2])
-print("url: " + api_url)
+
 print("status code: " + responseStatusCode)
 print("status: " + str(jsonResponse))
