93afd2ba04e455da8dbd0a30b66d231513c4ee6b
Abhijeet Bodhankar <48222006+abbodh@users.noreply.github.com>
2023-07-14 13:31:30 +0530

    Create args.py

---
+++ Release/23.2.0/LeadImport/args.py
@@ -0,0 +1,7 @@
+import argparse
+
+def argstoparse():
+    parser = argparse.ArgumentParser(allow_abbrev=False)
+    parser.add_argument('--environment', action='store', type=str, required=True)
+    args = parser.parse_args()
+return args
