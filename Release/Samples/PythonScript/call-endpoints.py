899ba0ae0b5b7e2c72b144666a769ec8c311f0a8
Abhijeet Bodhankar <48222006+abbodh@users.noreply.github.com>
2023-07-15 15:35:14 +0530

    Update call-endpoints.py

--- Release/Samples/PythonScript/call-endpoints.py
+++ Release/Samples/PythonScript/call-endpoints.py
@@ -6,12 +6,25 @@ arg = argstoparse()
 
 print("running for environment: " + arg.environment)
 
+# GET API sample
 api_url = "http://api.open-notify.org/astros.json?env=" + arg.environment
-print("hit url: " + api_url)
+print("get api url: " + api_url)
 
 response = requests.get(api_url)
 responseStatusCode = str(response.status_code)
 jsonResponse = response.json()
 
-print("status code: " + responseStatusCode)
-print("status: " + str(jsonResponse))
+print("get response status code: " + responseStatusCode)
+print("get response: " + str(jsonResponse))
+
+# POST API sample
+post_api_url = "https://jsonplaceholder.typicode.com/posts?env=" + arg.environment
+request_body = {
+    "title": "foo",
+    "body": "bar",
+    "userId": 1,
+  }
+
+print("post api url: " + post_api_url)
+post_response = requests.post(post_api_url, json=request_body)
+print("post api response: " + post_response.status_code)
